8af1c965e1a5f76d4965e37abf1c0e05872c3d23
vmishkovets <v.mishkovets@belitsoft.com>
2022-08-01 14:13:34 +0300

    add today date

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import { DateTime } from 'luxon'
 
+import './style.scss'
+
 export const Calendar = () => {
+  const WEEKDAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+  const weekday = WEEKDAYS[DateTime.now().weekday]
   const day = DateTime.now().day
   const month = DateTime.local().toLocaleString({ month: 'long' })
+
   return (
-    <div>
-      <div>{'Today'}</div>
-      <div>{day}</div>
-      <div>{month}</div>
+    <div className={'todayDate'}>
+      <div className={'dateItem'}>{weekday}</div>
+      <div className={'dateItem day'}>{day}</div>
+      <div className={'dateItem'}>{month}</div>
     </div>
   )
 }
